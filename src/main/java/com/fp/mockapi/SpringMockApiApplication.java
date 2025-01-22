08a11f1055a2ecf9a01b1e9a1f95534f7a9d6f6f
Ruben Leis <rubenleisfp@gmail.com>
2025-01-22 09:00:27 +0100

    First commit

---
+++ src/main/java/com/fp/mockapi/SpringMockApiApplication.java
@@ -0,0 +1,13 @@
+package com.fp.mockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMockApiApplication.class, args);
+	}
+
+}
